8f4e733453733923fbaf49b6db0fbfe98c91182c
Richter <richter@sear.local>
2018-01-09 10:32:44 +0100

    initial Commit

---
+++ DateCellSampleApp/DateCellSampleApp/Extenstions/DateTimeCell.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DateCellSampleApp.Extensions
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateTimeCell : ViewCell
+	{
+		public static readonly BindableProperty LabelProperty =
+			BindableProperty.Create("Label", typeof(string), typeof(DateTimeCell));
+
+		public string Label
+		{
+			get { return (string)GetValue(LabelProperty); }
+			set { SetValue(LabelProperty, value); }
+		}
+
+		public static readonly BindableProperty DateTimeProperty =
+			BindableProperty.Create("DateTime", typeof(DateTime), typeof(DateTimeCell), DateTime.Today);
+
+		public DateTime DateTime
+		{
+			get { return (DateTime)GetValue(DateTimeProperty); }
+			set { SetValue(DateTimeProperty, value); } 
+		}
+
+		public DateTimeCell ()
+		{
+			InitializeComponent ();
+
+			BindingContext = this;
+		}
+	}
+}
---
+++ DateCellSampleApp/DateCellSampleApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateCellSampleApp.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private DateTime _dateTime;
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+            set { SetProperty(ref _dateTime, value); }
+        }
+
+        private string _label;
+        public string Label
+        {
+            get { return _label; }
+            set { SetProperty(ref _label, value); }
+        }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+            Label = "TestText";
+            DateTime = new DateTime(1979, 4, 21, 23, 40, 0);
+        }
+    }
+}
